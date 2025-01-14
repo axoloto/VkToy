a67e14401fd8650a0a7c9f2170838553342c02f7
axoloto <moulin.adrien00@gmail.com>
2025-01-13 20:05:49 -0500

    Adding glm 3rd lib dep, removing glad

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,11 @@ class Conanfile(ConanFile):
     name = "VkToy"
     version = "0.0.1"
     requires = ["sdl/[==2.24]",
-                "glad/[==0.1.36]",
                 "spdlog/[==1.10.0]",
                 "vulkan-headers/[==1.3.239.0]",
                 "vulkan-loader/[==1.3.239.0]",
-                "imgui/[==1.89]"
+                "imgui/[==1.89]",
+                "glm/cci.20230113"
                 ]
     settings = "os", "compiler", "arch", "build_type"
     exports = "*"
