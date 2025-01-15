7bdf4b6c6e29248f4f030f12df8c977857e76f9f
axoloto <moulin.adrien00@gmail.com>
2025-01-14 22:39:23 -0500

    First glfw window

--- src/app/VkToyApp.cpp
+++ src/app/VkToyApp.cpp
@@ -1,31 +1,54 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
+static constexpr uint32_t WIDTH = 800;
+static constexpr uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication
 {
   public:
   void run()
   {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
   private:
+  GLFWwindow* window;
+
+  void initWindow()
+  {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
   void initVulkan()
   {
   }
 
   void mainLoop()
   {
+    while (!glfwWindowShouldClose(window))
+    {
+      glfwPollEvents();
+    }
   }
 
   void cleanup()
   {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
   }
 };
 
