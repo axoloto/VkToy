658945fad37896e9fcf8428bb0a474bfc4249142
axoloto <moulin.adrien00@gmail.com>
2025-03-01 13:42:15 -0500

    Adding missing new files, utils and shaders

---
+++ src/app/Utils.cpp
@@ -0,0 +1,30 @@
+#include "Utils.hpp"
+#include <fstream>
+#include <iostream>
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+std::vector<char> readShaderFile(const std::string& shaderName)
+{
+  return readFile(std::string(TOSTRING(SHADER_DIR)) + shaderName);
+}
+
+std::vector<char> readFile(const std::string& fileName)
+{
+  std::ifstream file(fileName, std::ios::ate | std::ios::binary);
+
+  if (!file.is_open())
+  {
+    throw std::runtime_error("Failed to open file!");
+  }
+
+  size_t fileSize = (size_t)file.tellg();
+  std::vector<char> buffer(fileSize);
+
+  file.seekg(0);
+  file.read(buffer.data(), fileSize);
+  file.close();
+
+  return buffer;
+}
---
+++ src/app/Utils.hpp
@@ -0,0 +1,5 @@
+#include <string>
+#include <vector>
+
+std::vector<char> readShaderFile(const std::string& shaderName);
+std::vector<char> readFile(const std::string& fileName);
