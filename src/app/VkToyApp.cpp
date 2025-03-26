21f4aeee62b54ec370b808cca5514614858d1175
axoloto <moulin.adrien00@gmail.com>
2025-03-25 22:18:50 -0400

    Adding index buffer

--- src/app/VkToyApp.cpp
+++ src/app/VkToyApp.cpp
@@ -69,13 +69,15 @@ struct Vertex
 };
 
 const std::vector<Vertex> vertices = {
-  { { 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f } },
-  { { 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f } },
-  { { -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f } }
+  { { -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f } },
+  { { 0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f } },
+  { { 0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f } },
+  { { -0.5f, 0.5f }, { 1.0f, 1.0f, 1.0f } }
 };
 
-VkResult
-CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
+const std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
 {
   auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
   if (func != nullptr)
@@ -132,6 +134,8 @@ class HelloTriangleApplication
 
   VkBuffer vertexBuffer;
   VkDeviceMemory vertexBufferMemory;
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
 
   const int MAX_FRAMES_IN_FLIGHT = 2;
   uint32_t currentFrame = 0;
@@ -175,6 +179,7 @@ class HelloTriangleApplication
     createFramebuffers();
     createCommandPool();
     createVertexBuffer();
+    createIndexBuffer();
     createCommandBuffers();
     createSyncObjects();
   }
@@ -877,6 +882,27 @@ class HelloTriangleApplication
     vkFreeMemory(device, stagingBufferMemory, nullptr);
   }
 
+  void createIndexBuffer()
+  {
+    VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), (size_t)bufferSize);
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+    copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+  }
+
   void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
   {
     VkBufferCreateInfo bufferInfo {};
@@ -1044,7 +1070,9 @@ class HelloTriangleApplication
     VkDeviceSize offsets[] = { 0 };
     vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
 
-    vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+    vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
+    vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
     vkCmdEndRenderPass(commandBuffer);
 
@@ -1239,6 +1267,9 @@ class HelloTriangleApplication
 
     vkDestroyBuffer(device, vertexBuffer, nullptr);
     vkFreeMemory(device, vertexBufferMemory, nullptr);
+
+    vkDestroyBuffer(device, indexBuffer, nullptr);
+    vkFreeMemory(device, indexBufferMemory, nullptr);
 
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
     {
