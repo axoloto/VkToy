c47bbad1c453ad1161a1af45f41e43bc86e90848
axoloto <moulin.adrien00@gmail.com>
2025-04-21 12:26:48 -0400

    Adjust res and rotation

--- src/app/VkToyApp.cpp
+++ src/app/VkToyApp.cpp
@@ -32,8 +32,8 @@
 #include "Logging.hpp"
 #include "Utils.hpp"
 
-static constexpr uint32_t WIDTH = 800;
-static constexpr uint32_t HEIGHT = 600;
+static constexpr uint32_t WIDTH = 1920;
+static constexpr uint32_t HEIGHT = 1080;
 
 const std::vector<const char*> validationLayers = {
   "VK_LAYER_KHRONOS_validation"
@@ -1956,7 +1956,7 @@ class HelloTriangleApplication
     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
     UniformBufferObject ubo {};
-    ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(10.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
     ubo.proj[1][1] *= -1;
